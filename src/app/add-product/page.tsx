068682356cb66aaebf38e8fbdb3f763568114e18
FardinSalehi <fardinsalehizadeh1998@gmail.com>
2023-11-13 10:58:02 +0330

    complete add product logic

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+import FormSubmitButton from "@/components/FormSubmitButton";
+
 export const metadata = {
   title: "Add Product - Flowmazon",
 };
 
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
 const AddProductPage = () => {
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-200 p-3">
       <h1 className="text-lg mb-3 font-bold">Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           type="text"
           required
@@ -35,9 +57,7 @@ const AddProductPage = () => {
           placeholder="Price"
           className="mb-3 w-full input input-bordered"
         />
-        <button type="submit" className="btn btn-primary btn-block">
-          Add Product
-        </button>
+        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
       </form>
     </div>
   );
---
+++ src/components/FormSubmitButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ComponentProps } from "react";
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+
+type FormSubmitButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+} & ComponentProps<"button">;
+
+const FormSubmitButton = ({
+  children,
+  className,
+  ...props
+}: FormSubmitButtonProps) => {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      {...props}
+      disabled={pending}
+      type="submit"
+      className={`btn btn-primary ${className}`}
+    >
+      {pending && <span className="loading loading-spinner" />}
+      {children}
+    </button>
+  );
+};
+
+export default FormSubmitButton;
